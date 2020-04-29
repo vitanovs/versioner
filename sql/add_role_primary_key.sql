c13b9546a9ba88ab6c11c8f9a5233356570e26cd
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2020-04-29 21:18:17 +0300

    chore/sql: Remove project use case specific migrations
    This commit removes project specific SQL files.

--- sql/add_role_primary_key.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION;
-
-CREATE OR REPLACE FUNCTION core.schema_version() RETURNS text AS $$ SELECT 'add_role_primary_key'::text; $$ IMMUTABLE LANGUAGE sql;
-
-ALTER TABLE core.role
-ADD CONSTRAINT role_pk PRIMARY KEY (id);
-
-COMMIT;
