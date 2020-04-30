05b66aba65e24657af5d68a901a10b9189f0aac6
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2020-04-30 23:34:52 +0300

    chore/cmd: Instrument `drop` command to use `logrus` logging

--- cmd/database_drop.go
+++ cmd/database_drop.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"github.com/vitanovs/versioner/client"
-	"github.com/vitanovs/versioner/log"
 )
 
 // NewDatabaseDropCommand returns a command
@@ -22,13 +22,20 @@ func NewDatabaseDropCommand() cli.Command {
 				Usage:    "the name of the database to drop",
 				Required: true,
 			},
+			cli.BoolFlag{
+				Name:  "debug",
+				Usage: "enable debug logging",
+			},
 		},
 	}
 }
 
 func runDatabaseDropCommand(ctx *cli.Context) error {
+	if ctx.Bool("debug") {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	cmdName := ctx.Command.Name
-	log.Info("Start '%s' command", cmdName)
 
 	name := ctx.String("name")
 	if name == "" {
@@ -37,31 +44,32 @@ func runDatabaseDropCommand(ctx *cli.Context) error {
 	}
 
 	runContext := context.Background()
+
+	log.Debug("Loading PostgreSQL client configuration...")
 	clientConfig, err := loadClientConfig(ctx)
 	if err != nil {
-		msg := fmt.Sprintf("Failed to client configuration: %s", err)
+		msg := fmt.Sprintf("failed to client configuration: %s", err)
 		return cli.NewExitError(msg, 1)
 	}
 
-	log.Info("Initializing database client...")
+	log.Debug("Initializing database client...")
 	psqlClient, err := client.NewClient(runContext, clientConfig)
 	if err != nil {
-		msg := fmt.Sprintf("Failed to initialize database client: %s", err)
+		msg := fmt.Sprintf("failed to initialize database client: %s", err)
 		return cli.NewExitError(msg, 1)
 	}
 
 	if _, err := psqlClient.DropDatabase(runContext, name); err != nil {
-		msg := fmt.Sprintf("Failed to drop database '%s': %s", name, err)
+		msg := fmt.Sprintf("failed to drop database '%s': %s", name, err)
 		return cli.NewExitError(msg, 1)
 	}
-	log.Info("Database '%s' was dropped", name)
+	log.Infof("Database '%s' was dropped", name)
 
-	log.Info("Closing database client...")
+	log.Debugf("Closing database client...")
 	if err := psqlClient.Close(); err != nil {
-		msg := fmt.Sprintf("Failed to close database client connection: %s", err)
+		msg := fmt.Sprintf("failed to close database client connection: %s", err)
 		return cli.NewExitError(msg, 1)
 	}
 
-	log.Info("Finish '%s' command", cmdName)
 	return nil
 }
